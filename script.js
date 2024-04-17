a0a11314c6169f3e3b482c29016f15aae90c5616
Your Name <you@example.com>
2024-04-17 09:52:13 +0200

    alakul a roller

--- script.js
+++ script.js
@@ -37,78 +37,74 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGameOver = false;
     let score = 0;
 
-    function control(event) {
+function control(event) {
         if(event.keyCode === 32){
-            if(!isJumping){
-                isJumping = true;
-                jump();
-            }
+        if(!isJumping){
+            isJumping = true;
+            jump();
         }
     }
+}
 
-    function jump(){
-        let count = 0;
-        let jumpTimerId = setInterval(() => {
-            if(count === 20){
-                clearInterval(jumpTimerId);
+function jump(){
+    let count = 5;
+    let jumpTimerId = setInterval(() => {
+        if(count == 20){
+            clearInterval(jumpTimerId)
 
-                let downTimerId = setInterval(() => {
-                    if(count === 0){
-                        clearInterval(downTimerId);
-                        isJumping = false;
-                    }
-                    position -= 5;
-                    position = position * gravity;
-                    dino.style.bottom = position + 'px';
-                    count--;
-                }, 20);
-            }
-            position += 30;
-            position = position * gravity;
-            dino.style.bottom = position + 'px';
-            count++;
-        },20);
+            let downTimerId = setInterval(() => {
+                if(count == 5){
+                    clearInterval(downTimerId);
+                    isJumping = false;
+                }
+                position -= 5;
+                position = position * gravity;
+                dino.style.bottom = position + 'px';
+                count --;
+            }, 20);
+        }
+        position += 30;
+        position = position * gravity;
+        dino.style.bottom = position + 'px';
+        count++;
+    },20);
+}
+
+function generateCactus() {
+    let randomTime = (Math.random() * 2000) + 500;
+    let cactusPosition = 1500;
+    let cactus = document.createElement('div');
+
+    if(!isGameOver){
+        cactus.classList.add('cactus');
+        cactus.style.left = cactusPosition + 'px';
+        grid.appendChild(cactus);
     }
 
-    function generateCactus() {
-        let randomTime = Math.random() * 2000 + 500;
-        let cactusPosition = 1500;
-        let cactus = document.createElement('div');
-
-        if(!isGameOver){
-            cactus.classList.add('cactus');
-            grid.appendChild(cactus);
+    let downTimerId = setInterval(() => {
+        if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
+            clearInterval(downTimerId);
+            alert.innerHTML = 'Game Over! Score: ' + score;
+            isGameOver = true;
+            while(grid.firstChild){
+                grid.removeChild(grid.lastChild);
+            }
         }
-
-        let moveCactusTimerId = setInterval(() => {
-            if(isGameOver){
-                clearInterval(moveCactusTimerId);
-            }
-            cactusPosition -= 10;
-            cactus.style.left = cactusPosition + 'px';
-
-            if(cactusPosition < -60){
-                clearInterval(moveCactusTimerId);
-                grid.removeChild(cactus);
-            }
-
-            if (cactusPosition > 0 && cactusPosition < 60 && position < 60){
-                clearInterval(moveCactusTimerId);
-                alert.innerHTML = 'Game Over! Score: ' + score;
-                isGameOver = true;
-            }
-
-            if(cactusPosition === 0){
-                score++;
-                alert.innerHTML = score;
-            }
-        }, 30);
-
-        if(!isGameOver){
-            setTimeout(generateCactus, randomTime);
-        }
+    if(cactusPosition == 0){
+        score ++;
+        alert.innerHTML = score;
     }
 
-    document.addEventListener('keyup', control);
-    generateCactus();
+    cactusPosition -= 10;
+    cactus.style.left = cactusPosition + 'px';
+    },30);
+
+    if(!isGameOver){
+        setTimeout(generateCactus, randomTime);
+    }
+}
+
+document.addEventListener('keyup', control)
+generateCactus();
 });
+
